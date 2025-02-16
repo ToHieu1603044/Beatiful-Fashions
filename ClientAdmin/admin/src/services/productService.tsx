23d59a2ff4e6468764a5eb0994a35835b79ca9f8
To Hieu <hieuttph47639@fpt.edu.vn>
2025-02-16 16:55:05 +0700

    Them san pham Fe va bien the + hien thi

---
+++ ClientAdmin/admin/src/services/productService.tsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const API_URL = "http://127.0.0.1:8000/api/products";
+
+export const getProducts = async (params?: { search?: string; category_id?: number }) => {
+  return await axios.get(API_URL, { params });
+};
+
+export const getProductById = async (id: number) => {
+  return await axios.get(`${API_URL}/${id}`);
+};
+
+export const createProduct = async (data: FormData) => {
+  return await axios.post(API_URL, data, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+};
+
+export const updateProduct = async (id: number, data: FormData) => {
+  return await axios.post(`${API_URL}/${id}?_method=PUT`, data, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+};
+
+export const deleteProduct = async (id: number) => {
+  return await axios.delete(`${API_URL}/${id}`);
+};
--- Server/app/Http/Controllers/Api/BrandController.php
+++ Server/app/Http/Controllers/Api/BrandController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\ApiResponse;
 use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
-    //
+    public function index()
+    {
+        $brands = \App\Models\Brand::all();
+        return ApiResponse::responseObject($brands);
+    }
 }
